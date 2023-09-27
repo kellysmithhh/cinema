903b01690b15172b5febe229ecf1b65882d0fdd8
Kelly Smith <kjs31537@uga.edu>
2023-09-27 15:46:22 -0400

    select seats and ages

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import HomePage from './components/HomePage/HomePage';
-// import OrderConfirmation from './components/OrderConfirmation/OrderConfirmation'
+import OrderConfirmation from './components/OrderConfirmation/OrderConfirmation'
 import CheckoutUI from './components/CheckoutUI/CheckoutUI';
+import TheaterBooking from './components/TheaterBooking/TheaterBooking';
 
 function App() {
   return (
     <div className="App">
       <HomePage />
+      <OrderConfirmation />
       <CheckoutUI />
+      <TheaterBooking />
     </div>
   );
 }
