72b7d29bff0b4a3794897dbab71669491c3f0bd7
Kelly Smith <kjs31537@uga.edu>
2023-09-28 12:06:43 -0400

    added ticket prices to theater booking

--- my-react-app/src/components/TheaterBooking/TheaterBooking.js
+++ my-react-app/src/components/TheaterBooking/TheaterBooking.js
@@ -35,11 +35,11 @@ function TheaterBooking() {
       </div>
         <div className="ticket-selector-border"> 
             <div className="ticket-selector">
-            <label htmlFor="adultTickets">Adult Tickets:</label>
+            <label htmlFor="adultTickets">Adult Tickets ($14.99):</label>
             <input type="number" id="adultTickets" name="adultTickets" min="0" />
-            <label htmlFor="childTickets">Child Tickets:</label>
+            <label htmlFor="childTickets">Child Tickets ($9.99):</label>
             <input type="number" id="childTickets" name="childTickets" min="0" />
-            <label htmlFor="seniorTickets">Senior Tickets:</label>
+            <label htmlFor="seniorTickets">Senior Tickets ($12.99):</label>
             <input type="number" id="seniorTickets" name="seniorTickets" min="0" />
             <button>Book Tickets</button>
         </div>
