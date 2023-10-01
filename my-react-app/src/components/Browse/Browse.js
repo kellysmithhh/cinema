44f02ddec0081289690d09c685a0296307ac13e4
Tucker Copeland <95252904+Tuck14@users.noreply.github.com>
2023-10-01 17:01:01 -0400

    Add files via upload

--- my-react-app/src/components/Browse/Browse.js
+++ my-react-app/src/components/Browse/Browse.js
@@ -1,12 +1,39 @@
 import './Browse.css';
+import React from "react"
+import { useState } from 'react';
+import { useEffect } from 'react';
   
 function Browse() {
+
+    const[movies,setMovies] = useState([])
+    const[userSearch,setSearch] = useState('')
+    
+    useEffect(()=> {
+        fetch("http://localhost:8080/movie/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setMovies(result);
+        }
+        )
+    },[])
+
+    const handleClick = (e) => {
+        movies.forEach(element => {
+            if (element.title == userSearch) {
+                return (
+                    <div>
+                        YEP!
+                    </div>
+                )
+            }
+        });
+    }
 
     return (
     
          <div className = "searchBar">
-            <input type='text' placeholder='Movie Name..'/>
-             <button type='submit'>Search</button>
+            <input type='text' placeholder='Movie Name..' id='input'value={userSearch} onChange={(e)=>setSearch(e.target.value)}/>
+             <button type='submit'onClick={handleClick}>Search</button>
         </div>
 
     );
