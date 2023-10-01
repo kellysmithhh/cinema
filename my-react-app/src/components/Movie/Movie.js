3e87c534c2a83c4ffdf7d1d7eca1a6314509f98c
wab025 <awb78727@uga.edu>
2023-10-01 16:19:45 -0400

    Created Movie Information
    Created Movie Information

--- my-react-app/src/components/Movie/Movie.js
+++ my-react-app/src/components/Movie/Movie.js
@@ -2,6 +2,7 @@ import './Movie.css';
 import React from "react"
 import { useState } from 'react';
 import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Movie() {
 
@@ -17,12 +18,20 @@ function Movie() {
         )
     },[])
 
+    let navigate = useNavigate(); 
+    const routeChange = () =>{ 
+        let path = `/MovieInformation`; 
+        navigate(path);
+    }
+
+    
+
     return (
         <div className="Movie">
             <div id = "MovieBlock">
-            
+
                 <div className = "movieImage">
-                
+                    <img src ="https://image.tmdb.org/t/p/original/u5kboZR4OMi4QdbOhawCZuzMVWJ.jpg" alt = "barbie poster"/>
                 </div>
 
                 <div className = "movieInfo">
@@ -34,7 +43,7 @@ function Movie() {
                     </div>
                     
                     <div className='button'>
-                        <button>Book Ticket</button>
+                        <button onClick = {routeChange} type="submit">More Information</button>
                     </div>
                     
                 </div>
