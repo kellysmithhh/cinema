725bad1f9d4e11758d1a443268bacf2a655355ca
wab025 <awb78727@uga.edu>
2023-10-01 22:04:29 -0400

    MovieGalleryEdits
    EDITs

--- my-react-app/src/components/Movie/Movie.js
+++ my-react-app/src/components/Movie/Movie.js
@@ -1,22 +1,8 @@
 import './Movie.css';
 import React from "react"
-import { useState } from 'react';
-import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Movie() {
-
-    const[movies,setMovies] = useState([])
-
-
-    useEffect(()=> {
-        fetch("http://localhost:8080/movie/getAll")
-        .then(res=>res.json())
-        .then((result)=>{
-            setMovies(result);
-        }
-        )
-    },[])
+function Movie(props) {
 
     let navigate = useNavigate(); 
     const routeChange = () =>{ 
@@ -24,22 +10,20 @@ function Movie() {
         navigate(path);
     }
 
-    
-
     return (
         <div className="Movie">
             <div id = "MovieBlock">
 
                 <div className = "movieImage">
-                    <img src ="https://image.tmdb.org/t/p/original/u5kboZR4OMi4QdbOhawCZuzMVWJ.jpg" alt = "barbie poster"/>
+                    <img src = {props.img} alt = "barbie poster"/>
                 </div>
 
                 <div className = "movieInfo">
-                    <h1>Barbie</h1>
+                    <h1>{props.title}</h1>
 
                     <div className = "rating">
-                        <h2>PG-13</h2>
-                        <h2>2h 13m</h2>
+                        <h2>{props.MPAA_rating_code}</h2>
+                        <h2>{props.catagory}</h2>
                     </div>
                     
                     <div className='button'>
--- my-react-app/src/components/MovieGallery/MovieGallery.js
+++ my-react-app/src/components/MovieGallery/MovieGallery.js
@@ -1,13 +1,28 @@
 import Movie from '../Movie/Movie';
 import './MovieGallery.css';
 
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 function MovieGallery() {
+
+    const[movies,setMovies] = useState([])
+
+    useEffect(()=> {
+        fetch("http://localhost:8080/movie/getAll")
+        .then(res=>res.json())
+        .then((result)=>{
+            setMovies(result);
+        }
+        )
+    },[])
 
     return (
         <div className="MovieGallery">
-            <Movie />
-            <Movie />
-            <Movie />
+
+        <Movie img = "https://image.tmdb.org/t/p/original/u5kboZR4OMi4QdbOhawCZuzMVWJ.jpg" title ="Barbie" MPAA_rating_code ="PG-13" catagory = "Romance"/>
+        <Movie img ="https://m.media-amazon.com/images/I/71xDtUSyAKL._AC_UF894,1000_QL80_.jpg" title = "Oppenheimer" MPAA_rating_code ="R"  catagory = "Action"/>          
+            
         </div>
     );
 
