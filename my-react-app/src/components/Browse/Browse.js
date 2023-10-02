25550bbe2e08e94db54ec4055d942912319615e1
Tucker Copeland <95252904+Tuck14@users.noreply.github.com>
2023-10-01 20:51:51 -0400

    Add files via upload

--- my-react-app/src/components/Browse/Browse.js
+++ my-react-app/src/components/Browse/Browse.js
@@ -5,35 +5,34 @@ import { useEffect } from 'react';
   
 function Browse() {
 
-    const[movies,setMovies] = useState([])
-    const[userSearch,setSearch] = useState('')
-    
-    useEffect(()=> {
-        fetch("http://localhost:8080/movie/getAll")
-        .then(res=>res.json())
-        .then((result)=>{
-            setMovies(result);
-        }
-        )
-    },[])
+    const[movies,setMovies] = useState([]);
+    const[userSearch,setSearch] = useState('');
 
-    const handleClick = (e) => {
-        movies.forEach(element => {
-            if (element.title == userSearch) {
-                return (
-                    <div>
-                        YEP!
-                    </div>
-                )
-            }
-        });
-    }
+        const handleClick = () => {
+        if (userSearch.trim() === '') {
+          return;
+        }
+    
+        const apiUrl = `http://localhost:8080/movie/search/${userSearch}`;
+    
+        fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+            setMovies(data);
+          })
+          .catch(error => {
+            console.error('Error fetching data:', error);
+          });
+      };
 
     return (
     
          <div className = "searchBar">
             <input type='text' placeholder='Movie Name..' id='input'value={userSearch} onChange={(e)=>setSearch(e.target.value)}/>
              <button type='submit'onClick={handleClick}>Search</button>
+             {movies.map(movie =>(
+                <h3>{movie.title} {movie.id}</h3>
+             ))}
         </div>
 
     );
