c764ee4b8c51413f06a66551485b8b20c5d02810
Tucker Copeland <tmc90644@uga.edu>
2023-10-22 16:24:03 -0400

    working on login

---
+++ cinemasystem/src/main/java/com/cinema/cinemasystem/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.cinema.cinemasystem.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.cinema.cinemasystem.model.User;
+import com.cinema.cinemasystem.service.UserService;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin(origins = "*")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public String add(@RequestBody User user) {
+        userService.saveMovie(user);
+        return "New user added.";
+    }
+
+    // @GetMapping("/getAll")
+    // public List<Movie> getAllMovies() {
+    //     return movieService.getAllMovies();
+    // }
+
+    @GetMapping("/search/{email}")
+    public List<User> getMoviesByTitle(@PathVariable String email) {
+        return userService.getMoviesByTitle(email);
+    }
+
+    @PostMapping("/login")
+
+}
