29c42a1569443f942f13e27273740da72f15f9a4
Tucker Copeland <tmc90644@uga.edu>
2023-10-24 21:10:45 -0400

    updated login

--- my-react-app/src/components/LoginPage/LoginPage.js
+++ my-react-app/src/components/LoginPage/LoginPage.js
@@ -7,28 +7,46 @@ function LoginPage() {
     const[email,setEmail] = useState('')
     const[password,setPassword] = useState('')
 
-    function generateCode() {
-        return Math.floor(1000 + Math.random() * 9000);
-    }
-
     let navigate = useNavigate();
 
-    const handleSignInClick = () => {
-
-        var sessionId = generateCode();
-        localStorage.setItem('sessionID',sessionId);
-        const apiUrl = `http://localhost:8080/user/login/${email}/${password}`
-        fetch(apiUrl, {
-            method:"POST",
-            headers:{"Content-Type":"application/json"}})
-            .then(()=>{console.log("")}) // check if login valid 
-            .catch(error => {
-                console.error('Error fetching data:', error);
-              });
-
-            //change path
-            let path = `/ManagerView`; 
-            navigate(path);
+    const handleSignInClick = (e) => {
+        e.preventDefault();
+        if(email.includes('@')) {
+            const apiUrl = `http://localhost:8080/user/login/${email}/${password}` // responds with sessionId if found in customer table else null
+            fetch(apiUrl, {
+                method:"POST",
+                headers:{"Content-Type":"application/json"}})
+                .then((response)=>response.text())
+                .then((data) => {
+                    if (data !== '') {
+                        let path = `/`;
+                        navigate(path);
+                    } else {
+                        alert("Email or password incorrect. Pleas try again.")
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        } else {
+            const apiUrl = `http://localhost:8080/user/login/admin/${email}/${password}` // responds with sessionId if found in customer table else null
+            fetch(apiUrl, {
+                method:"POST",
+                headers:{"Content-Type":"application/json"}})
+                .then((response)=>response.text())
+                .then((data) => {
+                    if (data !== '') {
+                        //change path
+                        let path = `/ManagerView`; 
+                        navigate(path);
+                    } else {
+                        alert("Id or password incorrect. Pleas try again.")
+                    }
+                }) 
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
         
     }
 
