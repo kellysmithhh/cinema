de0126fbd89a100f1e83cf4c06c4fe0f775bdded
Jon <68740476+jonbyte32@users.noreply.github.com>
2023-12-03 16:49:06 -0500

    a

--- cinemasystem/src/main/java/com/cinema/cinemasystem/service/MovieService.java
+++ cinemasystem/src/main/java/com/cinema/cinemasystem/service/MovieService.java
@@ -3,13 +3,9 @@ package com.cinema.cinemasystem.service;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
-import java.util.stream.Collectors;
 
-import org.aspectj.internal.lang.annotation.ajcPrivileged;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.cinema.cinemasystem.Repository.MovieRepository;
