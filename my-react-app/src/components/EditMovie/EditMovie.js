677186823132bf7a7f47c4b375d93fb00b725581
AlexLooney123 <looney1@uga.edu>
2023-12-05 01:31:35 -0500

    editmove

--- my-react-app/src/components/EditMovie/EditMovie.js
+++ my-react-app/src/components/EditMovie/EditMovie.js
@@ -5,6 +5,7 @@ function EditMovie(props) {
     const [dateTimeSet, setDateTimeSet] = useState([]);
     const [dateTimeValue, setDateTimeValue] = useState('');
     const [title] = useState(movie.movie.title);
+    //const [movieId] = useState(movie.movie.id)
     const [showRoomId, setShowRoomId] = useState(1);
 
     const handleDate = (e) => {
@@ -54,6 +55,24 @@ function EditMovie(props) {
         setShowRoomId(parseInt(e.target.value));
     };
 
+    const handleUpdateMovie = (e) => {
+        e.preventDefault();
+        const movieId = movie.movie.id;     
+        const comingSoon = false;
+        const nowShowing = true   
+        fetch(`http://localhost:8080/movie/update-status/${movieId}?comingSoon=${comingSoon}&nowShowing=${nowShowing}`,{ 
+            method:"POST",
+            headers:{"Content-Type":"application/json"},})
+        
+        .catch(rejected=> {
+            console.log(rejected);
+        })
+
+        
+        //fetch
+
+        console.log("Movie Updated");
+    }
 
     return (
         <table>
@@ -76,7 +95,7 @@ function EditMovie(props) {
                     <button type="submit">Send DateTime</button>
                 </form>
             </td>
-            <td><button id='editInfo'>Edit Info</button><button>Delete</button></td>
+            <td><button id='editInfo'onClick = {handleUpdateMovie}>Add to Currently Showing</button></td>
         </tr>
         </table>
     );
