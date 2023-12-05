8784c0ccc5b067ec31456c188da57862a3471e44
Tucker Copeland <tmc90644@uga.edu>
2023-12-05 00:31:00 -0500

    booking works

--- my-react-app/src/components/Orderhistory/OrderHistory.js
+++ my-react-app/src/components/Orderhistory/OrderHistory.js
@@ -1,6 +1,23 @@
-
+import React, { useEffect } from 'react';
 
 function OrderHistory() {
+
+    useEffect(() => {
+        var sessionId = localStorage.getItem('session');
+        sessionId = sessionId.replace(/^"(.*)"$/, '$1');
+        const apigetURL = `http://localhost:8080/user/booking/get/${sessionId}`;
+        fetch(apigetURL, {
+        method:"GET",
+        headers:{"Content-Type":"application/json"}})
+        .then((response)=> response.json())
+        .then((data) => { 
+            console.log(data);
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });  
+    }, []);
+
 
     return (
         <h2>Order History</h2>
